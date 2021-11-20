2a184e228872b09c199d8a5b049985c0b4f6ebc9
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-19 19:24:45 -0500

    Refactored day12, completed day13

---
+++ src/com/company/day13abstract_classes/Main.java
@@ -0,0 +1,17 @@
+package com.company.day13abstract_classes;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+
+}
