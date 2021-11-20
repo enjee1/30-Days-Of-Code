6652c3ac0bd369819bd0f7cfeaf49d8b74dd9585
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-19 19:58:46 -0500

    Added package organziation for day17

---
+++ src/com/company/day17more_exceptions/Calculator.java
@@ -0,0 +1,13 @@
+package com.company.day17more_exceptions;
+
+class Calculator {
+    public int power(int n, int p) throws Exception {
+
+        if (n < 0 || p < 0) {
+            throw new Exception("n and p should be non-negative");
+        } else {
+            return (int) Math.pow(n, p);
+        }
+
+    }
+}
