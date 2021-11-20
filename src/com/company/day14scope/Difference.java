61dee5055556f2cdfccf4dfa2a3898adea3e9b06
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-19 19:41:19 -0500

    Completed day 13 and day14 HackerRank challenges

---
+++ src/com/company/day14scope/Difference.java
@@ -0,0 +1,33 @@
+package com.company.day14scope;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    Difference(int[] numbers) {
+        this.elements = numbers;
+    }
+
+    public void computeDifference() {
+        int difference = 0;
+        for (int i = 0; i < elements.length ; i++) {
+            for (int j = i + 1; j < elements.length ; j++) {
+                difference = Math.abs(elements[i] - elements[j]);
+                if (difference > maximumDifference ) {
+                    maximumDifference = difference;
+
+                }
+            }
+        }
+
+    }
+
+
+}
